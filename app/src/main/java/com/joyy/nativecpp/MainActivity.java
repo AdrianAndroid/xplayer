14abaa882d3729ddbfcac495040c19f80cfe28db
zhaojian2 <zhaojian2@joyy.sg>
2021-10-13 10:28:38 +0800

    可以使用的SufraceView

--- app/src/main/java/com/joyy/nativecpp/MainActivity.java
+++ app/src/main/java/com/joyy/nativecpp/MainActivity.java
@@ -3,14 +3,19 @@ package com.joyy.nativecpp;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -59,10 +64,138 @@ public class MainActivity extends AppCompatActivity {
         initVideo();
 //        extracted();
 
+        initMediaPlayer();
+
         initTest();
 
         File file = new File(getExternalCacheDir(), "hello");
         file.mkdirs();
+    }
+
+    private final MediaPlayer mMediaPlayer = new MediaPlayer();
+
+    private void initMediaPlayer() {
+        binding.mFPlay.getHolder().setKeepScreenOn(true);
+        binding.mFPlay.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                readyPlay();
+                Toast.makeText(MainActivity.this, "readyPlay() done", Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+            }
+        });
+        mMediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+            @Override
+            public void onVideoSizeChanged(MediaPlayer mediaPlayer, int i, int i1) {
+                changeVideoSize();
+            }
+        });
+        binding.play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                play222();
+            }
+        });
+
+        binding.play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stop222();
+            }
+        });
+    }
+
+    public void changeVideoSize() {
+        int videoWidth = mMediaPlayer.getVideoWidth();
+        int videoHeight = mMediaPlayer.getVideoHeight();
+
+        int surfaceWidth = binding.mSurfaceView.getWidth();
+        int surfaceHeight = binding.mSurfaceView.getHeight();
+
+        float max;
+        if (getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            //竖屏模式下按视频宽度计算放大倍数值
+            max = Math.max((float) videoWidth / (float) surfaceWidth, (float) videoHeight / (float) surfaceHeight);
+        } else {
+            //横屏模式下按视频高度计算放大倍数值
+            max = Math.max(((float) videoWidth / (float) surfaceHeight), (float) videoHeight / (float) surfaceWidth);
+        }
+
+        //视频宽高分别/最大倍数值 计算出放大后的视频尺寸
+        videoWidth = (int) Math.ceil((float) videoWidth / max);
+        videoHeight = (int) Math.ceil((float) videoHeight / max);
+
+        //无法直接设置视频尺寸，将计算出的视频尺寸设置到surfaceView 让视频自动填充。
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(videoWidth, videoHeight);
+        params.addRule(RelativeLayout.CENTER_VERTICAL, binding.mContainer.getId());
+        binding.mSurfaceView.setLayoutParams(params);
+
+    }
+
+    public void readyPlay() {
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mMediaPlayer.setDataSource("/sdcard/Android/data/com.joyy.nativecpp/cache/v1080.mp4");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMediaPlayer.setLooping(true);
+        // 把视频画面输出到SurfaceView
+        mMediaPlayer.setDisplay(binding.mSurfaceView.getHolder());
+        // 通过异步的方式装载媒体资源
+        mMediaPlayer.prepareAsync();
+
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                //装载完毕回调
+                play222();
+                Toast.makeText(MainActivity.this, "mMediaPlayer.setOnPreparedListener", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mMediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                Toast.makeText(MainActivity.this, "onError", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    /**
+     * 播放或者暂停
+     */
+    private void play222() {
+        if (mMediaPlayer != null) {
+            if (mMediaPlayer.isPlaying()) {
+//                mMediaPlayer.pause();
+//                mBtnPlay.setText("播放");
+            } else {
+                mMediaPlayer.start();
+//                mBtnPlay.setText("暂停");
+            }
+        }
+    }
+
+    private void stop222() {
+        if (mMediaPlayer != null) {
+            if (mMediaPlayer.isPlaying()) {
+                mMediaPlayer.pause();
+//                mBtnPlay.setText("播放");
+            } else {
+//                mMediaPlayer.start();
+//                mBtnPlay.setText("暂停");
+            }
+        }
     }
 
     //OpenGlES sharder初始化完成并编译顶点和着色器代码
