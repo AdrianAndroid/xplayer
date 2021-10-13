589a49cff98606b8a41f7242b81ddeccb5a8e63f
zhaojian2 <zhaojian2@joyy.sg>
2021-10-13 16:22:36 +0800

    可以播放视频啦

--- app/src/main/java/com/joyy/nativecpp/yuv/YUVPlayer.java
+++ app/src/main/java/com/joyy/nativecpp/yuv/YUVPlayer.java
@@ -23,15 +23,15 @@ public class YUVPlayer extends GLSurfaceView implements Runnable, SurfaceHolder.
         setRenderer(new Renderer() {
             public void onSurfaceCreated(GL10 gl, EGLConfig config)
             {
-                gl.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+                //gl.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
             }
 
             public void onSurfaceChanged(GL10 gl, int w, int h) {
-                gl.glViewport(0, 0, w, h);
+                //gl.glViewport(0, 0, w, h);
             }
 
             public void onDrawFrame(GL10 gl) {
-                gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+                //gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
             }
         });
     }
