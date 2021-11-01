cbf69aad6871b39c1f63623e3704a17323217fea
zhaojian2 <zhaojian2@joyy.com>
2021-11-01 09:55:45 +0800

    添加注释

--- app/src/main/cpp/player/FFPlayerBuilder.cpp
+++ app/src/main/cpp/player/FFPlayerBuilder.cpp
@@ -14,32 +14,38 @@ void FFPlayerBuilder::InitHard(void *vm) {
     XLOGI("FFPlayerBuilder::InitHard(void *vm)");
 }
 
+// 播放器句柄
 IPlayer *FFPlayerBuilder::CreatePlayer(unsigned char index) {
     XLOGI("IPlayer *FFPlayerBuilder::CreatePlayer index=%d", index);
     return IPlayer::Get(index);
 }
 
+// 解析文件
 IDemux *FFPlayerBuilder::CreateDemux() {
     XLOGI("IDemux *FFPlayerBuilder::CreateDemux()");
     IDemux *ff = new FFDemux();
     return ff;
 }
 
+// 解码器
 IDecode *FFPlayerBuilder::CreateDecode() {
     IDecode *ff = new FFDecode();
     return ff;
 }
 
+// 重采样
 IResample *FFPlayerBuilder::CreateResample() {
     IResample *ff = new FFResample();
     return ff;
 }
 
+// 播放视频
 IVideoView *FFPlayerBuilder::CreateVideoView() {
     IVideoView *ff = new GLVideoView();
     return ff;
 }
 
+// 播放音频
 IAudioPlay *FFPlayerBuilder::CreateAudioPlay() {
     IAudioPlay *ff = new SLAudioPlay();
     return ff;
